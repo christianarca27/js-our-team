46a7470c932023c0287318928dab29394b477eb7
christianarca27 <christian.arca.mail@gmail.com>
2023-03-09 10:17:08 +0100

    refactor: spostato percorso immagini in un punto più comodo

--- js/script.js
+++ js/script.js
@@ -2,32 +2,32 @@ let members = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        photo: "../img/wayne-barnett-founder-ceo.jpg"
+        photo: "wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
         role: "Chief Editor",
-        photo: "../img/angela-caroll-chief-editor.jpg"
+        photo: "angela-caroll-chief-editor.jpg"
     },
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        photo: "../img/walter-gordon-office-manager.jpg"
+        photo: "walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        photo: "../img/angela-lopez-social-media-manager.jpg"
+        photo: "angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
         role: "Developer",
-        photo: "../img/scott-estrada-developer.jpg"
+        photo: "scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        photo: "../img/barbara-ramos-graphic-designer.jpg"
+        photo: "barbara-ramos-graphic-designer.jpg"
     }
 ];
 
@@ -45,7 +45,7 @@ for(let i = 0; i < members.length; i++) {
         let newDivEl;
         if(key == "photo") {
             newDivEl = document.createElement("img");
-            newDivEl.src = members[i][key];
+            newDivEl.src = "../img/" + members[i][key];
         }
         else {
             newDivEl = document.createElement("div");
